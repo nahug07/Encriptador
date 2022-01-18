886833a1a3275d25c86adf38c5bec2f6d8789623
Nahuel Grijalba <nahug07@gmail.com>
2022-01-18 17:52:44 -0300

    agregado de funcionalidad

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 /* Reglas de encriptación: 
 "e" es convertido para "enter" 
 "i" es convertido para "imes"
@@ -9,6 +8,22 @@ Solo letras minusculas
 No se permite acentuación de palabras 
 */
 
+var texto = document.getElementById("texto");
+var salida = document.getElementById("msg");
+var botonE = document.getElementById("botonE");
+
+botonE.addEventListener("click", clickE);
+
+function clickE() {
+  var cadena = document.getElementById("texto").value;
+  cadena = cadena.replace("e", "enter");
+  cadena = cadena.replace("i", "imes");
+  cadena = cadena.replace("a", "ai");
+  cadena = cadena.replace("o", "ober");
+  cadena = cadena.replace("u", "ufat");
+  document.getElementById("msg").value = cadena;
+}
+
 /* Reglas de desencriptación: 
 "enter" es convertido para "e" 
 "imes" es convertido para "i"
@@ -18,3 +33,27 @@ No se permite acentuación de palabras
 Solo letras minusculas
 No se permite acentuación de palabras   
 */
+
+botonD.addEventListener("click", clickD);
+
+function clickD() {
+  var cadena = document.getElementById("texto").value;
+  for (let i = 0; i < cadena.length; i++) {
+    cadena = cadena.replace("enter", "e");
+    cadena = cadena.replace("imes", "i");
+    cadena = cadena.replace("ai", "a");
+    cadena = cadena.replace("ober", "o");
+    cadena = cadena.replace("ufat", "u");
+    document.getElementById("msg").value = cadena;
+  }
+}
+
+/* --- Función de copiar --- */
+
+function copy() {
+  var copyText = document.querySelector("#msg");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+document.querySelector("#copy").addEventListener("click", copy);
